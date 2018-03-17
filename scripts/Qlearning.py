d602389319c54d0ecebb9c3743ac03ad5f916ecb
dudamarlena <marlena_duda@onet.eu>
2018-03-17 11:25:55 +0100

    Added reading data from excel and improved estimator for real players.

--- scripts/Qlearning.py
+++ scripts/Qlearning.py
@@ -1,18 +1,17 @@
-class Qlearning():
+class Qlearning(object):
     def __init__(self):
         self.Q_table = [0, 0, 0, 0, 0, 0]
-        self.alpha = 0.2
 
-    def update_q_table(self, left_card, right_card, decision, current_reward):
+    def update_q_table(self, left_card, right_card, decision, current_reward, alpha):
         if decision == 1:
-            updated_value = self.Q_table[left_card - 1] + self.alpha * (current_reward - self.Q_table[left_card - 1])
-            self.Q_table[left_card - 0] = updated_value
+            updated_value = self.Q_table[left_card - 1] + alpha * (current_reward - self.Q_table[left_card - 1])
+            self.Q_table[left_card - 1] = updated_value
         elif decision == 0:
-            updated_value = self.Q_table[right_card - 1] + self.alpha * (current_reward - self.Q_table[right_card - 1])
+            updated_value = self.Q_table[right_card - 1] + alpha * (current_reward - self.Q_table[right_card - 1])
             self.Q_table[right_card - 1] = updated_value
         return self.Q_table
 
 
 if __name__ == '__main__':
     ql = Qlearning()
-    print(ql.update_q_table(1, 2, 1, -1))
+    print(ql.update_q_table(1, 2, 1, -1, 0.1))
